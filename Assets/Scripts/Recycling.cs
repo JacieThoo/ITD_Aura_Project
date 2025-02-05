a04f0f9a715f783cccc5006bcfc31def0c71b2be
JacieThoo <s10260236@connect.np.edu.sg>
2025-02-06 01:29:02 +0800

    Added Recycling Interaction

---
+++ Assets/Scripts/Recycling.cs
@@ -0,0 +1,45 @@
+/*
+* Author: Jacie Thoo Yixuan
+* Date: 6/2/2025
+* Description: Recycling interaction 
+*/
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.XR.Interaction.Toolkit.Interactables;
+
+public class Recycling : MonoBehaviour
+{
+    /// <summary>
+    /// Stores the category of the recycling bin (MUST BE SAME AS TAG)
+    /// </summary>
+    public string category;
+
+    /// <summary>
+    /// Database to update aura
+    /// </summary>
+    public MyDatabase myDatabase;
+
+    /// <summary>
+    /// Checks tag of item that enters the trigger area
+    /// Adds aura if tag matches category
+    /// </summary>
+    /// <param name="other"></param>
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag(category))
+        {
+            Debug.Log("Correct category");
+            myDatabase.UpdateAura(40);
+
+            // So the player cannot keep looping 
+            other.gameObject.tag = "Untagged";
+        }
+        else
+        {
+            Debug.Log("Wrong category");
+        }
+    }
+}
