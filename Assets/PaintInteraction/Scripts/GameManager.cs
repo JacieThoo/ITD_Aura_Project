59c99e993a5b11a684c9b4894f7b31a0f72f0651
praiseee <S10255495@connect.np.edu.sg>
2025-02-05 22:08:08 +0800

    Updated painting activity aura into database

--- Assets/PaintInteraction/Scripts/GameManager.cs
+++ Assets/PaintInteraction/Scripts/GameManager.cs
@@ -10,81 +10,36 @@ using System.Collections;
 
 public class GameManager : MonoBehaviour
 {
-    /// <summary>
-    /// instance of the GameManager
-    /// </summary>
     public static GameManager Instance;
 
-    /// <summary>
-    /// Score variable
-    /// </summary>
-    private int score;
-
-    /// <summary>
-    /// Reference to the score text UI element
-    /// </summary>
-    private TMP_Text scoreText;
-
-    /// <summary>
-    /// Text to display on the whiteboard UI
-    /// </summary>
-    [SerializeField] private TMP_Text whiteBoardText;
-
-    /// <summary>
-    /// Pop-up canvas for the whiteboard UI
-    /// </summary>
+    [SerializeField] private TMP_Text warningText;
     [SerializeField] private GameObject WBPopUpCanvas;
 
-    // Awake is called when the script is initialized
-    public void Awake()
+    private void Awake()
     {
-        // Ensure that only one instance of GameManager exists
         if (Instance == null)
         {
-            Instance = this; 
+            Instance = this;
+            DontDestroyOnLoad(this.gameObject);
         }
         else
         {
-            Destroy(gameObject); 
+            Destroy(gameObject);
         }
     }
 
-    /// <summary>
-    /// Add Score and update UI 
-    /// </summary>
-    /// <param name="amount"></param>
-    public void AddScore(int amount)
-    {
-        score += amount; // Increment score by amount
-        Debug.Log("Score is " + score); // Updated score
-
-        scoreText.text = "Score: " + score; // Update score display
-    }
-
-    /// <summary>
-    /// Show Warning Text
-    /// </summary>
     public void ShowWarning()
     {
-        whiteBoardText.text = "Are you sure it is the right color?"; // Warning message
-        WBPopUpCanvas.SetActive(true); // Display the warning pop-up
+        if (warningText == null || WBPopUpCanvas == null) return;
 
-        // Start the coroutine to hide the warning after 3 seconds
-        StartCoroutine(HideShowWarning(3f));
+        warningText.text = "Wrong color used! ";
+        WBPopUpCanvas.SetActive(true);
+        StartCoroutine(HideShowWarning(2f));
     }
 
-    /// <summary>
-    /// Warning Text to disappear after a specified time
-    /// </summary>
-    /// <param name="delay"></param>
-    /// <returns></returns>
     private IEnumerator HideShowWarning(float delay)
     {
-        // Wait for delay
         yield return new WaitForSeconds(delay);
-
-        // Hide pop-up after the delay
         WBPopUpCanvas.SetActive(false);
     }
-
 }
