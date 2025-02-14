f31bbd02e8d895ffce9768642a38ce4a5f565925
JacieThoo <s10260236@connect.np.edu.sg>
2025-02-14 15:30:34 +0800

    Fix Painting and Door Audio

--- Assets/Scripts/HouseDoor.cs
+++ Assets/Scripts/HouseDoor.cs
@@ -6,16 +6,19 @@ public class HouseDoor : MonoBehaviour
 {
     public GameObject closedDoor;
     public GameObject openedDoor;
+    public AudioSource doorAudio;
 
     public void OpenDoor()
     {
         closedDoor.SetActive(false);
         openedDoor.SetActive(true);
+        doorAudio.Play();
     }
 
     public void CloseDoor()
     {
         closedDoor.SetActive(true);
         openedDoor.SetActive(false);
+        doorAudio.Play();
     }
 }
